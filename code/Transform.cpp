58a9aa99dd48bc9d59a11d5c34bc6305624f12f7
miguelguti <miguelgutierrezruano@gmail.com>
2023-05-08 19:07:18 +0200

    added transform

---
+++ code/Transform.cpp
@@ -0,0 +1,44 @@
+
+// Distributed under MIT License
+// @miguelgutierrezruano
+// 2023
+
+#include "Transform.h"
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace mg
+{
+	Transform::Transform()
+		: position(), rotation(), scale(1.f, 1.f, 1.f), parent(nullptr)
+	{
+
+	}
+
+	Transform::Transform(Transform* parentTransform)
+		: position(), rotation(), scale(1.f, 1.f, 1.f), parent(parentTransform)
+	{
+	}
+
+	Transform::Transform(Transform* parentTransform, const vec3 startPosition, const vec3 startRotation, const vec3 startScale)
+		: position(startPosition), rotation(startRotation), scale(startScale), parent(parentTransform)
+	{
+
+	}
+
+	mat4 Transform::get_matrix()
+	{
+		mat4 identity = mat4(1);
+		mat4 scalingMatrix = glm::scale(identity, scale);
+
+		// Rotate based on euler angles
+		mat4 xRotate = glm::rotate(identity, radians(rotation.x), vec3(1.f, 0.f, 0.f));
+		mat4 yRotate = glm::rotate(identity, radians(rotation.y), vec3(0.f, 1.f, 0.f));
+		mat4 zRotate = glm::rotate(identity, radians(rotation.z), vec3(0.f, 0.f, 1.f));
+
+		mat4 rotationMatrix = zRotate * yRotate * xRotate;
+
+		mat4 translationMatrix = glm::translate(identity, position);
+
+		return translationMatrix * rotationMatrix * scalingMatrix;
+	}
+}
---
+++ code/headers/Transform.h
@@ -0,0 +1,43 @@
+
+// Distributed under MIT License
+// @miguelgutierrezruano
+// 2023
+
+#pragma once
+
+#include <glm/glm.hpp>
+
+using namespace glm;
+
+namespace mg
+{
+	class Transform
+	{
+
+	private: 
+
+		Transform* parent;
+
+		vec3 position;
+		vec3 rotation;
+		vec3 scale;
+
+	public:
+
+		Transform();
+
+		Transform(Transform* parentTransform);
+
+		Transform(Transform* parentTransform, const vec3 startPosition, const vec3 startRotation, const vec3 startScale);
+
+		const vec3 get_position() { return position; }
+		const vec3 get_rotation() { return rotation; }
+		const vec3 get_scale() { return scale; }
+
+		void set_position(vec3 newPosition) { position = newPosition; }
+		void set_rotation(vec3 newRotation) { rotation = newRotation; }
+		void set_scale(vec3 newScale) { scale = newScale; }
+
+		mat4 get_matrix();
+	};
+}
